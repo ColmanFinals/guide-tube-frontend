b51dc58b74de66b8bc766f361c0345879d2c10b4
Bar Agmon <48044742+BarAgmon@users.noreply.github.com>
2024-08-12 22:58:04 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -13,8 +13,8 @@ if (isProduction) {
   serverConfig = {
     port: 443,
     https: {
-       key: fs.readFileSync(path.resolve('/cert/client-key.pem')),
-       cert: fs.readFileSync(path.resolve('/cert/client-cert.pem')),
+       key: fs.readFileSync(path.resolve('/cert/server.key')),
+       cert: fs.readFileSync(path.resolve('/cert/server.crt')),
      },
   };
 } else {
